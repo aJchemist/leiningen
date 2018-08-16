d3004c59aa42ddd75038e05bc02b26fa4399bd50
aJchemist <valchemist@me.com>
2018-08-17 00:30:56 +0900

    2.8.1-ue

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -1,10 +1,11 @@
-(defproject leiningen-core "2.8.1"
+(defproject leiningen-core "2.8.1-ue"
+  :plugins ^:replace [[lein-pprint "1.2.0" :hooks false :middleware false]]
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Library for core functionality of Leiningen."
   ;; If you update these, update resources/leiningen/bootclasspath-deps.clj too
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [bultitude "0.2.8" :exclusions [org.tcrawley/dynapath]]
                  [org.flatland/classlojure "0.7.1"]
                  [robert/hooke "1.3.0"]
